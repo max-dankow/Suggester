f23b1355ad781d56a0aa625de3d99244bd437c77
Maxim Dankovtsev <max.dankow@gmail.com>
2015-03-29 19:04:43 +0300

    No alloc in cmp.

--- suggester.cpp
+++ suggester.cpp
@@ -1,27 +1,24 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
+#include <string.h>
 #include "suggester.h"
+
+struct Heap_Element
+{
+    size_t bound_left, bound_right;
+    Word maximum;
+
+    bool operator ()(const Heap_Element &a, const Heap_Element &b)
+    {
+        return a.maximum.freq < b.maximum.freq;
+    }
+};
 
 bool operator<(const Word &a, const Word &b)
 {
     return a.text < b.text;
 }
-
-//lower_bound comparator
-/*struct CMP_Lower
-{
-    bool operator ()(const Word &element, const Word &key) const
-    {
-        if (element.text.size() < key.text.size())
-        {
-            return element.text < key.text;
-        }
-
-        std::string el_prefix = std::string(element.text, 0, key.text.size());
-
-        return el_prefix < key.text;
-    }
-};*/
 
 bool cmp_bin_lower(const Word &element, const Word &key)
 {
@@ -30,9 +27,7 @@ bool cmp_bin_lower(const Word &element, const Word &key)
         return element.text < key.text;
     }
 
-    std::string el_prefix = std::string(element.text, 0, key.text.size());
-
-    return el_prefix < key.text;
+    return strncmp(element.text.c_str(), key.text.c_str(), key.text.size()) < 0;
 }
 
 bool cmp_bin_upper(const Word &key, const Word &element)
@@ -42,12 +37,11 @@ bool cmp_bin_upper(const Word &key, const Word &element)
         return key.text < element.text;
     }
 
-    std::string el_prefix = std::string(element.text, 0, key.text.size());
-
-    return key.text < el_prefix;
+    return strncmp(element.text.c_str(), key.text.c_str(), key.text.size()) > 0;
 }
 
-void Suggester::print_voc(std::vector<Word>::iterator start, std::vector<Word>::iterator end)const
+void Suggester::print_voc(std::vector<Word>::iterator start,
+                          std::vector<Word>::iterator end)const
 {
     for (auto it = vocabulary.begin(); it != vocabulary.end(); ++it)
     {
@@ -62,36 +56,8 @@ void Suggester::print_voc(std::vector<Word>::iterator start, std::vector<Word>::
     std::cout << '\n';
 }
 
-
-/*Suggester::Suggester(const std::vector<Word> &input_vocabulary):
-    segment_tree(input_vocabulary, voc_max, neutral)
-{
-    vocabulary = input_vocabulary;
-    std::sort(vocabulary.begin(), vocabulary.end());
-
-    for (size_t i = 0; i < vocabulary.size(); ++i)
-    {
-        vocabulary[i].index = i;
-    }
-
-    Voc_Max voc_max;
-    Word neutral = {"", -1, 0};
-    segment_tree.init_tree(vocabulary, voc_max, neutral);
-}*/
-
 std::vector<Word> Suggester::find_k_maximum(size_t left, size_t right, int k)
 {
-    //element of heap with comparator
-    struct Node
-    {
-        size_t bound_left, bound_right;
-        Word maximum;
-        bool operator ()(const Node &a, const Node &b)
-        {
-            return a.maximum.freq < b.maximum.freq;
-        }
-    } comparator;
-
     std::vector<Word> answer;
     k = std::min(k, (int) (right - left + 1));
 
@@ -99,35 +65,40 @@ std::vector<Word> Suggester::find_k_maximum(size_t left, size_t right, int k)
         return answer;
 
     answer.reserve(k);
-    std::vector<Node> segment_heap(1);
-    segment_heap[0] = {left, right, segment_tree.count_RMQ(left, right)};
-    std::push_heap(segment_heap.begin(), segment_heap.end(), comparator);
+    std::priority_queue<Heap_Element, std::vector<Heap_Element>, Heap_Element> segment_heap;
+    segment_heap.push({left, right, segment_tree.count_RMQ(left, right)});
 
     for (int i = 0; i < k; ++i)
     {
-        Node current_node = segment_heap.front();
-
-        std::pop_heap(segment_heap.begin(), segment_heap.end(), comparator);
-        segment_heap.pop_back();
+        Heap_Element current_node = segment_heap.top();
+        segment_heap.pop();
 
         answer.push_back(current_node.maximum);
 
-        Word sub_left = segment_tree.count_RMQ(current_node.bound_left, current_node.maximum.index - 1);
-        Word sub_right = segment_tree.count_RMQ(current_node.maximum.index + 1, current_node.bound_right);
+        Word sub_left = segment_tree.count_RMQ(current_node.bound_left,
+                                               current_node.maximum.index - 1);
 
-        Node push_node;
+        Word sub_right = segment_tree.count_RMQ(current_node.maximum.index + 1,
+                                                current_node.bound_right);
+
+        Heap_Element push_node;
+
         if (sub_left.freq != -1)
         {
-            push_node = {current_node.bound_left, current_node.maximum.index - 1, sub_left};
-            segment_heap.push_back(push_node);
-            std::push_heap(segment_heap.begin(), segment_heap.end(), comparator);
+            push_node = {current_node.bound_left,
+                         current_node.maximum.index - 1,
+                         sub_left};
+
+            segment_heap.push(push_node);
         }
 
         if (sub_right.freq != -1)
         {
-            push_node = {current_node.maximum.index + 1, current_node.bound_right, sub_right};
-            segment_heap.push_back(push_node);
-            std::push_heap(segment_heap.begin(), segment_heap.end(), comparator);
+            push_node = {current_node.maximum.index + 1,
+                         current_node.bound_right,
+                         sub_right};
+
+            segment_heap.push(push_node);
         }
     }
 
@@ -138,17 +109,15 @@ std::vector<Word> Suggester::make_suggest(std::string prefix, int suggest_number
 {
     Word to_search = {prefix, 0, 0};
 
-    //CMP_Lower cmp_lower;
-    std::vector<Word>::iterator start = std::lower_bound(vocabulary.begin(),
-                                                         vocabulary.end(),
-                                                         to_search, cmp_bin_lower);
-    //CMP_Upper cmp_upper;
-    std::vector<Word>::iterator end = std::upper_bound(vocabulary.begin(),
-                                                       vocabulary.end(),
-                                                       to_search, cmp_bin_upper);
-    std::vector<Word> range(start, end);
+    auto start = std::lower_bound(vocabulary.begin(),vocabulary.end(),
+                                  to_search, cmp_bin_lower);
+
+    auto end = std::upper_bound(vocabulary.begin(), vocabulary.end(),
+                                to_search, cmp_bin_upper);
 
     print_voc(start, end);
 
-    return find_k_maximum(start - vocabulary.begin(), end - vocabulary.begin() - 1, suggest_number);
+    return find_k_maximum(start - vocabulary.begin(),
+                          end - vocabulary.begin() - 1,
+                          suggest_number);
 }
